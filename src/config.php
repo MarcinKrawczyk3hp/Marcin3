8958a765eaad751fe7bed4a6c835bb6801b0b491
Marcin <pinfix01orginal@gmail.com>
2023-03-22 14:47:53 +0100

    User Działa :3

--- src/config.php
+++ src/config.php
@@ -1,9 +1,8 @@
 <?php
-
 require_once('./../vendor/autoload.php');
 $db = new mysqli("localhost", "root", "", "cms");
 require("Post.class.php");
-
-$loader = new Twig\Loader\FilesystemLoader('./../src/templates');
+require("User.class.php");
+$loader = new Twig\Loader\FilesystemLoader("./../src/templates");
 $twig = new Twig\Environment($loader);
 ?>
